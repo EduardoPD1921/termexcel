b0c0a045304b793a439929259bbee5fda92fcb95
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2023-02-10 00:25:03 -0300

    :bug: fixed problem where multiple functions were looping the lines of the same buffer

--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,59 @@ fn main() {
         Ok(f) => f,
         Err(_) => panic!("Erro ao abrir o arquivo.")
     };
-    let (file_as_vec, biggest_cell_len) = convert_file_into_vec_with_usize(file);
-}
 
-fn convert_file_into_vec_with_usize(file: File) -> (Vec<String>, usize) {
     let lines = BufReader::new(file).lines();
-    let mut formatted_cells_as_vec: Vec<String> = Vec::new();
-    let mut biggest_cell_len: usize = 0;
+    let mut raw_file_vec: Vec<String> = Vec::new();
 
     for line in lines {
         let l = line.unwrap();
+        raw_file_vec.push(l);
+    }
+
+    let biggests_cells_len_vec = get_vec_with_biggests_cells(&raw_file_vec);
+    let formatted_file_vec = convert_file_into_vec_with_usize(&raw_file_vec);
+
+    println!("{:?}", biggests_cells_len_vec);
+    println!("{:?}", formatted_file_vec);
+}
+
+fn convert_file_into_vec_with_usize(raw_file_vec: &Vec<String>) -> Vec<String> {
+    let mut formatted_cells_vec: Vec<String> = Vec::new();
+
+    for l in raw_file_vec {
         let splitted_cells: Vec<&str> = l.split(';').collect();
         let mut formatted_cell_vec: Vec<String> = Vec::new();
 
-        for l in splitted_cells {
-            if l.len() != 0 {
-                set_biggest_cell(&mut biggest_cell_len, l);
-                formatted_cell_vec.push(format!("{}|", l));
-            }
+        for cell in splitted_cells {
+            formatted_cell_vec.push(format!("{}|", cell));
         }
 
         let formatted_cell_slice: Vec<&str> = formatted_cell_vec.iter().map(|str| str.as_str()).collect();
         let formatted_cell_string = formatted_cell_slice.concat().to_owned();
-        formatted_cells_as_vec.push(formatted_cell_string);
+        formatted_cells_vec.push(formatted_cell_string);
     }
-    (formatted_cells_as_vec, biggest_cell_len)
+    formatted_cells_vec
 }
 
-fn set_biggest_cell(current_big_cell: &mut usize, cell: &str) {
-    if cell.len() > *current_big_cell {
-        *current_big_cell = cell.len();
+fn get_vec_with_biggests_cells(raw_file_vec: &Vec<String>) -> Vec<usize> {
+    let mut biggests_cells_len: Vec<usize> = Vec::new();
+
+    for l in raw_file_vec {
+        let splitted_cells: Vec<&str> = l.split(';').collect();
+
+        for (index, l) in splitted_cells.iter().enumerate() {
+            set_biggest_cell(&mut biggests_cells_len, l, index)
+        }
+    }
+    biggests_cells_len
+}
+
+fn set_biggest_cell(current_biggests_cells: &mut Vec<usize>, cell: &str, index: usize) {
+    if current_biggests_cells.get(index).is_some() {
+        if cell.len() > current_biggests_cells[index] {
+            current_biggests_cells[index] = cell.len();
+        }
+    } else {
+        current_biggests_cells.push(cell.len());
     }
 }
